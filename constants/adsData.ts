4334d0a2ea0372205bb8cd60f5fd84484ae15c85
kirtan0101 <kjpatel200022@gmail.com>
2025-09-09 13:59:47 +0530

    Remove unused mock data files for ads, orders, products, and pickups to clean up the codebase

--- constants/adsData.ts
+++
@@ -1,6 +0,0 @@
-export const ads = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMJ8y9Dc3T3JQBmWGRFkG_I_LtkdQd5G5M40H-WC2XIm-cL1QiZi0_vcsgXJ2Dm1AnA3pDqe9BDm3L3nuD9Kva5dh_UitNIo3-qNwG9f9JBg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6pgx2DQ-e_zq4-JpC6YlSJbq6zIm_fpU0iPrfDpWbyIqkAC3LawF4ICRZ7mUIeyN1rms&usqp=CAU",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTseTxt3mMhPZTO4CEXQdiR4009mPVHiBgK-oVDrmZD0wsD3_d6FMBxCfxdhZO6appquLg&usqp=CAU",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZqFA6LRlcqp3l21dDpJDgn7yfkKNdGAoodioA-bvQt_WClpvCCSETNZgRHTsmJpjmasA&usqp=CAU"
-]
